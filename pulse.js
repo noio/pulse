f2875ca133527704328a86139ef3d4b980e86b36
Thomas van den Berg <paranoid.ndroid@gmail.com>
2013-11-05 00:59:56 +0100

    working midi through socket.io

--- pulse.js
+++ pulse.js
@@ -1,5 +1,77 @@
 var pulse = function(module){
 
+	module.MIDI_CLOCK = 248;
+	module.PPQN = 24;
 	
+	module.beats = [];
+	module.bpm = 120;
+	module.mspb = 2000; 
+
+	var socket = null;
+	var clocks = 0;
+	var latest = 0;
+
+	function clock(){
+		if (clocks == 0){
+			module.tap()
+		}
+		clocks ++;
+		if (clocks == module.PPQN){
+			clocks = 0;
+		}
+	}
+
+	module.tap = function(){
+		module.beats.push((new Date).getTime());
+		if (module.beats.length > 5){
+			module.beats.shift()
+			module.mspb = (module.beats[4] - module.beats[0]) / 4;
+			// convert 'milliseconds per beats to 'beats per minute'
+			module.bpm = 60000 / module.mspb;
+		}
+	}
+
+	/**
+	* Connect to a pulse server, get the socket.io script
+	* and list its devices.
+	*/
+	module.connect = function(server, callback){
+		var script = document.createElement('script');
+		script.src = server + '/socket.io/socket.io.js';
+		console.log(script)
+		
+		script.onload = function () {	
+    		socket = io.connect(server);
+  			socket.on('devices', function (devices) {
+    			if (callback){
+    				callback(devices);
+    			}
+  			});
+		};
+
+		document.head.appendChild(script);
+	}
+
+	/**
+	* Subscribe to a specific device
+	*/
+	module.subscribe = function(id){
+		socket.emit('subscribe', 0);
+		socket.on('midi', function (data) {
+    		if (data == module.MIDI_CLOCK){
+    			clock();
+    		}
+    		
+  		});
+	}
+
+	/**
+	* Disconnect from the pulse server
+	*/
+	module.disconnect = function(){
+		socket.disconnect();
+	}
+
+	return module;
 
 }({})
--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-var connect = require('connect'),
-    socketio = require('socket.io'),
-    midi = require('midi.io');
+var midi = require('midi');
+var io = require('socket.io').listen(9000);
 
-var server = connect.createServer(),
-    io = socketio.listen(server);
+// Set up a new input.
+var input = new midi.input();
 
-server.use(midi(io));
-server.listen(9000);
+var devices = [];
+for (var i = 0; i < input.getPortCount(); i ++){
+	devices[i] = input.getPortName(i);
+}
+
+console.log("Devices: " + devices);
+
+io.sockets.on('connection', function (socket) {
+
+	socket.emit('devices', devices);
+
+	var midiReceived = function(deltaTime, message){
+		socket.emit('midi', message);
+	}
+
+	socket.on('subscribe', function(deviceID){
+		input.openPort(deviceID);
+		input.ignoreTypes(false, false, false);
+		input.on('message', midiReceived);	
+	});
+
+	socket.on('disconnect', function(reason){
+		console.log("Disconnected: " + reason);
+		input.removeListener('message', midiReceived);
+	});
+
+	
+});
