04f4ccff466556d50ee086c29f0bcaf1a18eae9c
Guy John <guy@rumblesan.com>
2014-11-03 23:18:48 +0000

    Adding semicolons to emit.js

--- emit.js
+++ emit.js
@@ -9,7 +9,7 @@ if (process.argv.length < 3){
 // Set up a new input.
 var output = new midi.output();
 var count = 0;
-var last = new Date().getTime()
+var last = new Date().getTime();
 
 // Create a virtual input port.
 output.openVirtualPort("A Beat!");
@@ -19,19 +19,19 @@ output.sendMessage([250]);
 function beat(){
     output.sendMessage([248]);
 
-    if (count % 48 == 0){ console.log('BOOM! ' + count/24) }
-    if (count % 48 == 24){ console.log("CLAP! " + count/24) }
+    if (count % 48 == 0){ console.log('BOOM! ' + count/24); }
+    if (count % 48 == 24){ console.log("CLAP! " + count/24); }
 
     count ++;
     if (count >= 24 * 16){
         count = 0;
         output.sendMessage([250]);
     }
-    var now = new Date().getTime()
+    var now = new Date().getTime();
     setTimeout(beat, last + 60000 / bpm / 24 - now);
     last += 60000 / bpm / 24;
 }
-beat()
+beat();
 
 // Close the port when done.
 output.closePort();
